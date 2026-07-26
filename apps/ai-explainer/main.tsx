1619fb5de369205132aa7c152d8e10a36dc92d95
rayanfer32 <rayanfernandes9@gmail.com>
2026-07-26 10:23:00 +0530

    refactor: move ai-explainer from demos/ to apps/

--- apps/ai-explainer/main.tsx
+++ apps/ai-explainer/main.tsx
@@ -4,4 +4,4 @@ import { mount } from "@pocketjs/framework";
 import AiExplainerSection2 from "./section2.tsx";
 import AiExplainerIntro from "./intro.tsx";
 
-mount(() => <AiExplainerIntro />);
+mount(() => <AiExplainerSection2 />);
